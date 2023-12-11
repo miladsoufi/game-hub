fbd6d148b48c1436c23354bab6fb008cae184d76
milad <milad.habibisoofi@gmail.com>
2023-12-11 11:35:25 +0330

    changing gamecard background color.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,18 +1,25 @@
-import React from "react";
+import {
+  Card,
+  CardBody,
+  HStack,
+  Heading,
+  Image,
+  useColorMode,
+} from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
-import PlatformIconList from "./PlatformIconList";
-import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import CriticScore from "./CriticScore";
 import Emoji from "./Emoji";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
+  const { toggleColorMode, colorMode } = useColorMode();
   return (
-    <Card>
+    <Card bg={colorMode === "light" ? "#d3d3d3" : undefined}>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <HStack justifyContent="space-between" marginBottom={3}>
